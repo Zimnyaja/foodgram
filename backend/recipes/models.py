86c967d32b0f06310f14f3e1bc789d3e3fb22c1c
Yuliya Tsaryk <koshka_j@bk.ru>
2025-07-19 02:31:23 +0300

    fixed flake

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -14,10 +14,14 @@ User = get_user_model()
 class BaseUserRecipe(models.Model):
     """Базовый класс для моделей, связывающих пользователя и рецепт."""
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, verbose_name="Пользователь", related_name="%(class)s_set",
+        User, on_delete=models.CASCADE,
+        verbose_name="Пользователь",
+        related_name="%(class)s_set",
     )
     recipe = models.ForeignKey(
-        'Recipe', on_delete=models.CASCADE, verbose_name="Рецепт", related_name="%(class)s_set",
+        'Recipe', on_delete=models.CASCADE,
+        verbose_name="Рецепт",
+        related_name="%(class)s_set",
     )
 
     class Meta:
